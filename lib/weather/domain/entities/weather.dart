8ca2023912469a16f7e17387cb8e8e508c67217f
devmoaid1 <moaidmohamed1312@gmail.com>
2022-10-24 19:07:57 +0200

    created weather entity

---
+++ lib/weather/domain/entities/weather.dart
@@ -0,0 +1,14 @@
+class Weather {
+  final int id;
+  final String cityName;
+  final String main;
+  final String description;
+  final int pressure;
+
+  Weather(
+      {required this.id,
+      required this.cityName,
+      required this.main,
+      required this.description,
+      required this.pressure});
+}
